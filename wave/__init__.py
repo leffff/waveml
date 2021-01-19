81044617e32e5503518702455571d063e71b9cdd
leffff <levnovitskiy@gmail.com>
2021-01-19 20:54:35 +1000

    Pypi preparation

--- wave/__init__.py
+++ wave/__init__.py
@@ -1,6 +1,5 @@
 from wave.main import WaveRegressor
 
-
 __author__ = 'Lev Novitskiy > levnovitskiy@gmail.com > https://github.com/leffff/ > https://www.kaggle.com/levnovitskiy'
 __license__ = 'Apache'
 __version__ = '2.0'
