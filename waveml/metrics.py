9c6439a88f8601edd5cb72cbf08d33ab73ae6012
leffff <levnovitskiy@gmail.com>
2021-01-27 22:11:49 +1000

    New loss functions added: SSE, SAE

--- waveml/metrics.py
+++ waveml/metrics.py
@@ -16,11 +16,24 @@ def MSE(y_true, y_pred):
     return error
 
 
+def SSE(y_true, y_pred):
+    error = torch.sum((y_true - y_pred) ** 2)
+    return error
+
+
 def MAE(y_true, y_pred):
     error = torch.mean(torch.abs(y_true - y_pred))
+    return error
+
+def SAE(y_true, y_pred):
+    error = torch.sum(torch.abs(y_true - y_pred))
     return error
 
 
 def MAPE(y_true, y_pred):
     error = torch.mean(torch.abs((y_true - y_pred) / y_true))
+    return error
+
+def MBE(y_true, y_pred):
+    error = torch.sum((y_true - y_pred) / y_true)
     return error
